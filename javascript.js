0687eff7d6e63754602b4430c2386eb048efb4f3
josecadima123 <112897771+josecadima123@users.noreply.github.com>
2022-09-05 15:39:28 -0400

    ejercicio 2 y 3
    se han actualizado el ejercicio 2 y 3

--- javascript.js
+++ javascript.js
@@ -3,5 +3,17 @@ function convertirQuintales(){
     var kilogramos=100*quintales;
     var texto=kilogramos;
     document.getElementById('kilogramos').value=texto;
-    
+}
+function convertirFarenheit(){
+    var farenheit=parseFloat(document.getElementById('farenheit').value);
+    var celsius=(farenheit-32)/1.8;
+    var texto=celsius;
+    document.getElementById('celsius').value=texto;
+}
+function calcularArea(){
+    var base=parseFloat(document.getElementById('base').value);
+    var altura=parseFloat(document.getElementById('altura').value);
+    var area=base*altura/2;
+    var texto=area;
+    document.getElementById('area').value=texto;
 }
